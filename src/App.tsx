f5bfa427aa15b768d8c84976fc96f0753a107cd9
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-05-28 23:06:03 -0300

    Primeiro commit (Preparando o setup do projeto)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+
+export function App() {
+  const foo = 'bar';
+
+  return (
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
