eb4e0cbf8688d149c87cac2135c17feeb3ccb4c1
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-06-16 22:38:43 -0300

    Preparando setup de testes (vitest e happy-dom)

---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitest/config';
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./test/setup.ts'],
+  },
+});
