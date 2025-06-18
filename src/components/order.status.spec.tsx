7751a21cd6f6c906d23e599b28afb5aa0741be46
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-06-17 22:58:25 -0300

    Testes unitários: Order Status e Pagination

--- src/components/order.status.spec.tsx
+++ src/components/order.status.spec.tsx
@@ -1,3 +1,54 @@
-test('1 + 1 equals 2', () => {
-  expect(1 + 1).toEqual(2);
+import { render } from '@testing-library/react';
+import { OrderStatus } from './order-status';
+
+describe('Order Status', () => {
+  it('should display the right text based on order status is pending', () => {
+    const wrapper = render(<OrderStatus status="pending" />);
+
+    const statusText = wrapper.getByText('Pendente');
+    const bagdeElement = wrapper.getByTestId('badge');
+
+    expect(statusText).toBeInTheDocument();
+    expect(bagdeElement).toHaveClass('bg-slate-400');
+  });
+
+  it('should display the right text based on order status is canceled', () => {
+    const wrapper = render(<OrderStatus status="canceled" />);
+
+    const statusText = wrapper.getByText('Cancelado');
+    const bagdeElement = wrapper.getByTestId('badge');
+
+    expect(statusText).toBeInTheDocument();
+    expect(bagdeElement).toHaveClass('bg-rose-500');
+  });
+
+  it('should display the right text based on order status is delivering', () => {
+    const wrapper = render(<OrderStatus status="delivered" />);
+
+    const statusText = wrapper.getByText('Entregue');
+    const bagdeElement = wrapper.getByTestId('badge');
+
+    expect(statusText).toBeInTheDocument();
+    expect(bagdeElement).toHaveClass('bg-emerald-500');
+  });
+
+  it('should display the right text based on order status is delivering', () => {
+    const wrapper = render(<OrderStatus status="delivering" />);
+
+    const statusText = wrapper.getByText('Em entrega');
+    const bagdeElement = wrapper.getByTestId('badge');
+
+    expect(statusText).toBeInTheDocument();
+    expect(bagdeElement).toHaveClass('bg-amber-500');
+  });
+
+  it('should display the right text based on order status is delivering', () => {
+    const wrapper = render(<OrderStatus status="processing" />);
+
+    const statusText = wrapper.getByText('Em preparo');
+    const bagdeElement = wrapper.getByTestId('badge');
+
+    expect(statusText).toBeInTheDocument();
+    expect(bagdeElement).toHaveClass('bg-amber-500');
+  });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
+import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['./test/setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
